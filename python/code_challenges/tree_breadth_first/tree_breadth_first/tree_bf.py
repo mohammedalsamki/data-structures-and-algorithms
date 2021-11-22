f3441d009b76f1e204690ec81d0b3fa69ab9d1b9
mohammedalsamki <momosamak21@gmail.com>
2021-11-22 18:40:14 -0500

    solve cc17

---
+++ python/code_challenges/tree_breadth_first/tree_breadth_first/tree_bf.py
@@ -0,0 +1,34 @@
+class Queue:
+  def __init__(self, collection=[]):
+    self.data = collection
+
+  def peek(self):
+    if len(self.data):
+      return True
+    return False
+
+  def enqueue(self,item):
+    self.data.append(item)
+
+  def dequeue(self):
+    return self.data.pop(0)
+
+def tree_bfs(tree):
+    """
+    function which returns a list of items in a given tree using bfs algorithm
+    Arguments: tree
+    Return: list of all values in the tree, in the order they were encountered
+    """
+
+    breadth = Queue()
+    breadth.enqueue(tree.root)
+
+    while breadth.peek():
+      front = breadth.dequeue()
+      yield front.data
+
+      if front.left:
+        breadth.enqueue(front.left)
+
+      if front.right:
+        breadth.enqueue(front.right)
